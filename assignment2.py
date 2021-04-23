33c0b2bde0fe3e27ade7d65859bf6e4055529d2a
Sohima <54314395+Sohima@users.noreply.github.com>
2021-04-23 20:00:18 +0530

    Add files via upload
    a.    Huffman Coding
    
    b.    LZW Coding

---
+++ assignment2.py
@@ -0,0 +1,112 @@
+string = 'aabbbaaccb'
+# Creating tree nodes
+class NodeTree(object):
+
+    def __init__(self, left=None, right=None):
+        self.left = left
+        self.right = right
+
+    def children(self):
+        return (self.left, self.right)
+
+    def nodes(self):
+        return (self.left, self.right)
+
+    def __str__(self):
+        return '%s_%s' % (self.left, self.right)
+# Main function implementing huffman coding
+def huffman_code_tree(node, left=True, binString=''):
+    if type(node) is str:
+        return {node: binString}
+    (l, r) = node.children()
+    d = dict()
+    d.update(huffman_code_tree(l, True, binString + '0'))
+    d.update(huffman_code_tree(r, False, binString + '1'))
+    return d
+
+
+# Calculating frequency
+freq = {}
+for c in string:
+    if c in freq:
+        freq[c] += 1
+    else:
+        freq[c] = 1
+
+freq = sorted(freq.items(), key=lambda x: x[1], reverse=True)
+
+nodes = freq
+
+while len(nodes) > 1:
+    (key1, c1) = nodes[-1]
+    (key2, c2) = nodes[-2]
+    nodes = nodes[:-2]
+    node = NodeTree(key1, key2)
+    nodes.append((node, c1 + c2))
+
+    nodes = sorted(nodes, key=lambda x: x[1], reverse=True)
+
+huffmanCode = huffman_code_tree(nodes[0][0])
+
+print(' Char | Huffman-code ')
+
+for (char, frequency) in freq:
+    print(' %-4r |%12s' % (char, huffmanCode[char]))
+
+#LZW Coing
+def compress(uncompressed):
+    """Compress a string to a list of output symbols."""
+
+    # Build the dictionary.
+    dict_size = 256
+    dictionary = dict((chr(i), chr(i)) for i in range(dict_size))
+    # in Python 3: dictionary = {chr(i): chr(i) for i in range(dict_size)}
+
+    w = ""
+    result = []
+    for c in uncompressed:
+        wc = w + c
+        if wc in dictionary:
+            w = wc
+        else:
+            result.append(dictionary[w])
+            # Add wc to the dictionary.
+            dictionary[wc] = dict_size
+            dict_size += 1
+            w = c
+
+    # Output the code for w.
+    if w:
+        result.append(dictionary[w])
+    return result
+
+compressed = compress('aabbbaaccb')
+print(compressed)
+
+#LZW Coding
+def decompress(compressed):
+    """Decompress a list of output ks to a string."""
+
+    # Build the dictionary.
+    dict_size = 256
+    dictionary = dict((chr(i), chr(i)) for i in range(dict_size))
+    # in Python 3: dictionary = {chr(i): chr(i) for i in range(dict_size)}
+
+    w = result = compressed.pop(0)
+    for k in compressed:
+        if k in dictionary:
+            entry = dictionary[k]
+        elif k == dict_size:
+            entry = w + w[0]
+        else:
+            raise ValueError('Bad compressed k: %s' % k)
+        result += entry
+
+        # Add w+entry[0] to the dictionary.
+        dictionary[dict_size] = w + entry[0]
+        dict_size += 1
+
+        w = entry
+    return result
+decompressed = decompress(compressed)
+print (decompressed)
